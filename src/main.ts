318ae76f45c34d1138f5991c5f67354a7ec46d73
Afo <obaone01@gmail.com>
2023-01-05 23:30:53 -0800

    intial files using vite for build

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+class App {
+  content: any = document.getElementById("content");
+
+  constructor() {
+    
+    console.log("Hello World!");
+  }
+}
+
+new App();
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,js}", "index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
