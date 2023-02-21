c7f9a7fdda2b170c4bb99f84e34c81aaf35ebbd9
Afo <obaone01@gmail.com>
2023-02-20 18:23:23 -0800

    moved images to nested folder

---
+++ src/assets/wefa/index.js
@@ -0,0 +1,17 @@
+export { default as sliceAppLarge } from './slice-app-large.jpg';
+export { default as sliceAppPlaceholder } from './slice-app-placeholder.jpg';
+export { default as sliceApp } from './slice-app.jpg';
+export { default as sliceBackgroundLarge } from './slice-background-large.jpg';
+export { default as sliceBackgroundPlaceholder } from './slice-background-placeholder.jpg';
+export { default as sliceBackground } from './slice-background.jpg';
+export { default as sliceIrlPlaceholder } from './slice-irl-placeholder.jpg';
+export { default as sliceIrl } from './slice-irl.jpg';
+export { default as sliceSidebarAnnotationsLarge } from './slice-sidebar-annotations-large.png';
+export { default as sliceSidebarAnnotationsPlaceholder } from './slice-sidebar-annotations-placeholder.png';
+export { default as sliceSidebarAnnotations } from './slice-sidebar-annotations.png';
+export { default as sliceSidebarLayersLarge } from './slice-sidebar-layers-large.png';
+export { default as sliceSidebarLayersPlaceholder } from './slice-sidebar-layers-placeholder.png';
+export { default as sliceSidebarLayers } from './slice-sidebar-layers.png';
+export { default as sliceSlidesLarge } from './slice-slides-large.jpg';
+export { default as sliceSlidesPlaceholder } from './slice-slides-placeholder.jpg';
+export { default as sliceSlides } from './slice-slides.jpg';
