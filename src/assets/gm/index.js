a1713b8b2d734a4d098cb55e39c25e6fef219a92
Afo <obaone01@gmail.com>
2023-02-22 18:05:09 -0800

    updates

--- src/assets/gm/index.js
+++ src/assets/gm/index.js
@@ -3,7 +3,7 @@ export { default as GMBackgroundImg } from './gm-background.png';
 export { default as GMDevelopmentImg } from './gm-development.png';
 export { default as GMDevelopment2Img } from './gm-development2.png';
 export { default as GMLearningsImg } from './gm-learnings.png';
-export { default as GMProblemImg } from './gm-problem.webp';
+export { default as GMProblemImg } from './gm-problem.png';
 
 export { default as gmBackgroundImgLarge } from './spr-background-large.jpg';
 export { default as gmBackgroundImgPlaceholder } from './spr-background-placeholder.jpg';
