252c50127bd28a26435bac6f2af38e50dc11d61f
Afo <obaone01@gmail.com>
2023-02-23 18:31:24 -0800

    added new images for projects and moved to typescript

--- src/assets/mira-flow/index.js
+++
@@ -1,7 +0,0 @@
-export { default as FlowArchitectureImg } from './flow-architecture.png'
-export { default as FlowArchitecture2Img } from './flow-architecture2.png'
-export { default as FlowBackgroundImg } from './flow-background.png'
-export { default as FlowDevelopmentImg } from './flow-development.png'
-export { default as FlowDevelopment2Img } from './flow-development2.png'
-export { default as FlowLearningsImg } from './flow-learnings.png'
-export { default as FlowProblemImg } from './flow-problem.webp'
---
+++ src/assets/wefa/index.ts
@@ -0,0 +1,11 @@
+export { default as WefaBackgroundImg } from './wefa-background.jpg'
+
+export { default as sliceSidebarAnnotationsLarge } from './slice-sidebar-annotations-large.png'
+export { default as sliceSidebarAnnotationsPlaceholder } from './slice-sidebar-annotations-placeholder.png'
+export { default as sliceSidebarAnnotations } from './slice-sidebar-annotations.png'
+export { default as sliceSidebarLayersLarge } from './slice-sidebar-layers-large.png'
+export { default as sliceSidebarLayersPlaceholder } from './slice-sidebar-layers-placeholder.png'
+export { default as sliceSidebarLayers } from './slice-sidebar-layers.png'
+export { default as sliceSlidesLarge } from './slice-slides-large.jpg'
+export { default as sliceSlidesPlaceholder } from './slice-slides-placeholder.jpg'
+export { default as sliceSlides } from './slice-slides.jpg'
