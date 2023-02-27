8bc71fccaab04cf8cc7b06841ba49677d156e355
Afo <obaone01@gmail.com>
2023-02-27 01:55:29 -0800

    added sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -40,3 +47,9 @@ module.exports = {
     return config
   },
 }
+
+module.exports = withSentryConfig(
+  module.exports,
+  { silent: true },
+  { hideSourcemaps: true }
+)
